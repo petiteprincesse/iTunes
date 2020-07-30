7269f1ef0466e0343d5ebc1dd09ffbe92e213b8b
Princess <Olgavm2007@yandex.ru>
2020-07-30 23:34:10 +0300

    fourth step

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,3 +1,5 @@
+import {addZero} from './supScript.js';
+
 export const videoPlayerInit = () => {
   const videoPlayer = document.querySelector('.video-player');
   const videoButtonPlay = document.querySelector('.video-button__play');
@@ -29,8 +31,6 @@ export const videoPlayerInit = () => {
     videoPlayer.currentTime = 0;
   };
 
-  const addZero = n => n < 10 ? '0' + n : n;
-
   videoPlayer.addEventListener('click', togglePlay);
   videoButtonPlay.addEventListener('click', togglePlay);
 
@@ -45,11 +45,11 @@ export const videoPlayerInit = () => {
 
     videoProgress.value = (currentTime / duration) * 100;
 
-    let minutePassed = Math.floor(currentTime / 60);
-    let secondsPassed = Math.floor(currentTime % 60);
+    let minutePassed = Math.floor(currentTime / 60) || '0';
+    let secondsPassed = Math.floor(currentTime % 60) || '0';
 
-    let minuteTotal = Math.floor(duration / 60);
-    let secondsTotal = Math.floor(duration % 60);
+    let minuteTotal = Math.floor(duration / 60) || '0';
+    let secondsTotal = Math.floor(duration % 60) || '0';
 
     videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`; 
     videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`; 
@@ -60,5 +60,12 @@ export const videoPlayerInit = () => {
     const value = videoProgress.value;
 
     videoPlayer.currentTime = (value * duration) / 100;
+  });
+
+  audioProgress.addEventListener('click', event => {
+    const x = event.offsetX;
+    const allWidth = audioProgress.clientWidth;
+    const progress = (x / allWidth) * audioPlayer.duration;
+    audioPlayer.currentTime = progress;
   });
 };
